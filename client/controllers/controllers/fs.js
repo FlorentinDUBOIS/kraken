1fd346d8fc51572d58815284345cef2346bc787e
Florentin DUBOIS <contact@florentin-dubois.fr>
2016-05-23 09:34:00 +0200

    improve navigation

--- client/controllers/controllers/fs.js
+++ client/controllers/controllers/fs.js
@@ -3,11 +3,44 @@
 filemanager.controller( 'filemanager.fs', ['$scope', '$http', '$translate', '$mdToast', function( $scope, $http, $translate, $mdToast ) {
     $scope.foldersSelected = [];
     $scope.filesSelected   = [];
+    $scope.path            = '/';
+
+    $scope.formatPath = function( path ) {
+        path = path.replace( /\/\//gi, '/' );
+
+        var dirs = path.split( '/' );
+        var data = [];
+        for( var i = 0; i < dirs.length; ++i ) {
+            if( i +1 < dirs.length ) {
+                if( dirs[i+1] == '..' ) {
+                    continue;
+                }
+            }
+
+            if( dirs[i] == '..' ) {
+                continue;
+            }
+
+            data.push( dirs[i] );
+        }
+
+        path = data.join( '/' );
+        if( path == '' ) {
+            path = '/';
+        }
+
+        return path;
+    };
 
     $scope.location = function( path ) {
+        path = $scope.formatPath( path );
+
         $http.get( '/fs/info' + path ).then( function( res ) {
-            $scope.folders = res.data.folders;
-            $scope.files   = res.data.files;
+            $scope.folders         = res.data.folders;
+            $scope.files           = res.data.files;
+            $scope.foldersSelected = [];
+            $scope.filesSelected   = [];
+            $scope.path            = path;
         }, function() {
             $translate( 'request.failure' ).then( function( trad ) {
                 $mdToast.showSimple( trad );
